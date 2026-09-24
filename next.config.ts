fe26eb61bc260350ef619ab38c8695a5e5b284f4
Leonard Viola <leonardviola20-wq@users.noreply.github.com>
2026-09-24 11:07:01 +0800

    Update next.config.ts with correct domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  // ✅ Force HTTPS for all routes
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        destination: "https://hrdb-lenard.vercel.app/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
