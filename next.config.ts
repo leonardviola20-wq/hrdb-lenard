8e5ddb8a8efb32be65677943366b52949acf53fe
Leonard Viola <leonardviola20-wq@users.noreply.github.com>
2026-09-24 11:09:47 +0800

    Update next.config.ts with correct domain

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/(.*)",
-        destination: "https://hrdb-lenard.vercel.app/:path*",
+        destination: "https://hrdb-lenard.vercel.app",
         permanent: true,
       },
     ];
